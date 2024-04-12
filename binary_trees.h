4d7e59bfceb8c24cc7e4d4bd678c0c68e9523762
basma ebraheim <basmaebraheim@gmail.com>
2024-04-12 23:57:24 +0200

    add header

--- binary_trees.h
+++ binary_trees.h
@@ -80,15 +80,6 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-int binary_tree_is_bst(const binary_tree_t *tree);
-bst_t *bst_insert(bst_t **tree, int value);
-bst_t *array_to_bst(int *array, size_t size);
-bst_t *bst_search(const bst_t *tree, int value);
 
-avl_t *rebalance(avl_t *node, avl_t **tree);
-avl_t *balance_left(avl_t *node);
-avl_t *balance_right(avl_t *node);
-char *convert(unsigned long int num, int base, int lowercase);
-bst_t *swap(bst_t *a, bst_t *b);
 
 #endif /* BINARY_TREES_H */
